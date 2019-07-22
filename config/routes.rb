502a76e9abe209e327960337f10eb4eea7054dba
PJToews <lohasjean@gmail.com>
2019-07-22 18:11:59 +0000

    Allow recipes creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :recipes, only: [:index, :show]
   namespace :author do
     resources :recipes, only: [:new, :create, :show]
   end
