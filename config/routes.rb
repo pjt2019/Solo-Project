f576fd64f63a39f022a9a5cd9d84e3dfe94b58be
PJToews <lohasjean@gmail.com>
2019-07-26 00:21:49 +0000

    Setting category link

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :recipes, only: [:index, :show]
-  namespace :author do
-    resources :recipes, only: [:new, :create, :show]
-  end
-  resources :categories, only: [:new, :create, :show]
+  resources :recipes #, only: [:index, :show]
+  resources :categories #, only: [:new, :create, :show]
 end
