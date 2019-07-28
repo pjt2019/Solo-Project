fad5e6b33232f67bf8b28bbf7e2042b2a266cab2
PJToews <lohasjean@gmail.com>
2019-07-28 04:23:44 +0000

    Setting up recipe creation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_categories
+  has_many :user_recipes
   has_many :categories, through: :recipe_categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_190550) do
+ActiveRecord::Schema.define(version: 2019_07_27_203532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2019_07_27_190550) do
 
   create_table "user_categories", force: :cascade do |t|
     t.integer "category_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_recipes", force: :cascade do |t|
+    t.integer "recipe_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
