b926d1b719d6f26de78c2e7c1f818c955e57a5eb
PJToews <lohasjean@gmail.com>
2019-07-29 20:34:37 +0000

    Adding recipe image to categories show page

---
+++ db/migrate/20190727203532_create_user_recipes.rb
@@ -0,0 +1,9 @@
+class CreateUserRecipes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_recipes do |t|
+      t.integer :recipe_id
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
