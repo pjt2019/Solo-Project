641352c51240bfc01db2c9ae62d9dfcf6baa95a8
PJToews <lohasjean@gmail.com>
2019-08-01 22:35:28 +0000

    Allowing recipe belong to multiple categories

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_categories
   has_many :user_recipes
+  has_many :categories
   has_many :categories, through: :recipe_categories
   mount_uploader :photo, PhotoUploader
 end
