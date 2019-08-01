d3486302d03457d73d28225f7eded381715dc1d5
PJToews <lohasjean@gmail.com>
2019-08-01 02:37:15 +0000

    Adding category image button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_034021) do
+ActiveRecord::Schema.define(version: 2019_07_31_205545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_07_31_034021) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "photos", force: :cascade do |t|
