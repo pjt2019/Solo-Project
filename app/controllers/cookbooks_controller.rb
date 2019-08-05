0a8807c3a0940ed9d90c64dba7882e5d7b74a32e
PJToews <lohasjean@gmail.com>
2019-08-05 21:59:20 +0000

    Adding cookbook show route

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -1,4 +1,5 @@
 class CookbooksController < ApplicationController
+  before_action :authenticate_user!
   def index
     @categories = Category.all
   end
@@ -8,7 +9,19 @@ class CookbooksController < ApplicationController
     @photo = Photo.new
   end
 
+  def create
+    current_user.cookbooks.create(recipe: current_recipe)
+    redirect_to cookbook_path(current_recipe)
+  end
+
   def add_recipe
     @ecipe = params
+  end
+
+
+  private
+
+  def current_recipe
+    @current_recipe ||= Recipe.find(params[:recipe_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   resources :recipes do #, only: [:index, :show]
     resources :photos, only: :create
+    
   end
-  resources :categories #, only: [:new, :create, :show]
-  resources :cookbooks
+  resources :categories #, only: [:new, :create, :show] 
+
+  resources :cookbooks, only: [:index, :create, :show]
 end
