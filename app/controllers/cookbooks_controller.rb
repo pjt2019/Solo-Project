1164c4081c1bed2e467e56aa054f710bd6eb5e26
PJToews <lohasjean@gmail.com>
2019-08-08 01:54:51 +0000

    Setting up cookbook pages

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -1,7 +1,26 @@
 class CookbooksController < ApplicationController
   before_action :authenticate_user!
   def index
-    @categories = Category.all
+    #give the new cookbook to the current user
+    if current_user.cookbook.nil?
+      new_cookbook = Cookbook.create(:user_id => current_user)
+      current_user.cookbook = new_cookbook      
+    end      
+    all_recipes = current_user.cookbook.recipes
+    all_categories = []
+    all_recipes.each do |recipe|
+      recipe.categories.each do |category|
+        all_categories << category
+      end
+    end
+    all_categories.uniq!
+    @categories = all_categories
+
+    # We know the current user
+    # Get their cookbook
+    # For each recipe in their cookbook, check all the categories that recipe has
+    # make a list of all the categories for all the recipes in the cookbook
+    # show the user all the unique categories from that list
   end
 
   def show
